385455a333c04055b7d6d7c8fe6876c0ac15b648
Tomás Pica <tomaspica31@gmail.com>
2022-06-30 17:30:31 -0300

    arreglo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/* 
-function saludar() {
+
+/* function saludar() {
     alert("Bienvenido " + usuario)
 };
 
@@ -11,62 +11,82 @@ let apellidoDeUsuario = prompt("Apellido");
 
 let usuario = (nombreDeUsuario + " " + apellidoDeUsuario);
 
-saludar();
+saludar(); */
 
+let autos = [
+  {
+    "id":"001",
+    "auto":"Honda Civic",
+    "precio":"50.000",
+    "img":"./img/hondacivic.png",
+    "desc":""
+  },
+  {
+    "id":"002",
+    "auto":"Ford Mondeo",
+    "precio":"90.000",
+    "img":"./img/fordmondeo.png",
+    "desc":""
+  },
+  {
+    "id":"003",
+    "auto":"Chevrolet Cruze",
+    "precio":"70.000",
+    "img":"./img/cruze.png",
+    "desc":""
+  },
+];
 
-class Auto {
-    constructor(id, nombre, precio) {
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-    }
-       
+let favoritos;
+
+if(JSON.parse(localStorage.getItem('favoritos'))) {
+  favoritos = JSON.parse(localStorage.getItem('favoritos'))
+} else {
+  localStorage.setItem('favoritos', JSON.stringify([]))
+  favoritos = JSON.parse(localStorage.getItem('favoritos'))
 }
 
-const Auto1 = new Auto(1, "Honda Civic ", 50000);
-const Auto2 = new Auto(2, "Ford Mondeo ", 70000);
- 
-let Autos = []
-Autos.push(Auto1);
-Autos.push(Auto2);
+function desplegarAutos() {
 
- 
-
-let opcion = parseInt(prompt("Elige un auto:  1. Honda Civic  2. Ford Mondeo"))
-
-for (let p of Autos) {
-    if (p.id == opcion) {
-        alert("Elegiste $ " + p.nombre + p.precio)
-    }
-}
-
-alert("Alquilaste tu auto " + usuario)
- */
-
-
-let carrito = {
-    1: { nombre: "Honda Civic" },
-    2: { nombre: "Ford Mondeo" },
-  };
-  for (const key in carrito) {
-    if (carrito.hasOwnProperty(key)) {
-      const element = carrito[key];
-      console.log(element);
-    }
+  for (let i = 0; i < autos.length; i++) {
+    const element = autos[i];
+    const { id, auto, precio, img } = element
+    const card = `
+    <div class='card row justify-content-center  mb-5 p-3'>
+      <div>
+       <img class='imgAuto' src=${img} width='350px' alt=''/>
+      </div>
+      <p>${auto}</p>
+      <div>
+        <p>$${precio.toLocaleString()}</p>
+      </div>
+      <div class="btn-container">
+        <button id=${id} class='btnAgregar'>AGREGAR A FAVORITOS</button>
+      </div>
+    </div>
+    `
+    const container = document.getElementById('container')
+    container.innerHTML += card
   }
+}
+
+desplegarAutos()
+
+const btnAgregar = document.getElementsByClassName('btnAgregar')
+
+for (let i = 0; i < btnAgregar.length; i++) {
+  const element = btnAgregar[i];
+  element.addEventListener('click', agregarAFavoritos)
   
-  Object.values(carrito).forEach((item) => console.log(item));
-  
-  console.log(carrito[1]);
-  [
-    {
-      "precio": 50.000,
-      "id": 1,
-      "title": "Honda Civic",
-    },
-    {
-      "precio": 70.000,
-      "id": 2,
-      "title": "Ford Mondeo",
-    },
-]
+}
+
+function agregarAFavoritos(e) {
+  const btn = e.target;
+  const idBoton = btn.getAttribute('id')
+  const autoEncontrado = autos.find(aut => aut.id == idBoton)
+  console.log(autoEncontrado)  
+}
+
+const contador = document.getElementById('favCounter')
+contador.innerHTML = favoritos.length
+
